5b4c1b7f114005559a29084ffab2bf4fcc5efac5
Yitamon <claret0127@gmail.com>
2018-03-22 10:13:17 -0500

    Update demographic.R

--- Yitao/demographic.R
+++ Yitao/demographic.R
@@ -1,15 +1,19 @@
+# MATH571 final project
+# Demographic analysis
+# Yitao Ma
 
 install.packages("devtools")
 library(devtools)
 install_github('arilamstein/choroplethrZip@v1.5.0')
 library(choroplethrZip)
 
+# This data contains all zip code in America
 data(df_pop_zip)
-
 
 data(df_zip_demographics)
 colnames(df_zip_demographics)
 
+# This function can highlight designated place on map
 highlight_zip = function(zip)
 {
   library(choroplethrZip)
@@ -21,8 +25,10 @@ highlight_zip = function(zip)
 
 df_zip_demographics$value <- df_zip_demographics[,2]
   
+# zip code of water plants
 water_zip <- c("60628","60804","60067","60018","60193", "60133","60439","60076")
 
+# create map
 choro <- zip_choropleth(df_zip_demographics, 
                        title="\n\nCook County ZCTA Demographics ", 
                        legend = "Population\n",
