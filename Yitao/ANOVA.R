c606ac5b913ebfa04417f1043bc4dd4a931ad15b
Yitamon <claret0127@gmail.com>
2018-03-22 13:04:20 -0500

    Update ANOVA.R

--- Yitao/ANOVA.R
+++ Yitao/ANOVA.R
@@ -11,13 +11,15 @@
 water_df <- read.csv("~/Desktop/ALL_MWRD.csv",  header=TRUE, sep=",")
 head(water_df)
 
+# change data type: factor -> numeric
 water_df[,'TKN'] <- as.character(water_df[,'TKN'])
 water_df[,'TKN'] <- as.numeric(water_df[,'TKN'])
 
-sort(water_df$TKN, decreasing = T)
-
+# change data type: numeric -> factor 
+# for boxplot 
 water_df[,'LOCATION'] <- as.factor(water_df[,'LOCATION'])
 
+# do blox plot
 library(ggplot2)
 # Location: 1=calumet, 2=egan,3=hanover park,
 #           4=kirie,5=lemont,6=obrien,7=southwest,8=west side
@@ -27,7 +29,7 @@ bplot <- ggplot(water_df, aes(x = LOCATION, y=TKN,fill=LOCATION)) +
                    labels=c("Calumet", "Egan", "Hanover Park","Kirie","Lemont","Obrien"))
 print(bplot)
 
-
+# outliers visualization
 hist <- ggplot(data=water_df, aes(water_df$TKN)) + 
   geom_histogram(col="black", 
                  aes(fill=..count..), 
@@ -36,11 +38,9 @@ hist <- ggplot(data=water_df, aes(water_df$TKN)) +
                  labs(x="TKN(mg/L)", y="Count")
 print(hist)
 
+# remove outliers by condition
 new_water_df <- water_df[water_df[, "TKN"] <= 50,]
 
-
-anova_R <- aov(TKN ~ LOCATION, data = new_water_df)
-summary(anova_R)
 
 
 new_bplot <- ggplot(new_water_df, aes(x = LOCATION, y=TKN,fill=LOCATION)) + 
@@ -48,7 +48,6 @@ new_bplot <- ggplot(new_water_df, aes(x = LOCATION, y=TKN,fill=LOCATION)) +
   scale_x_discrete(breaks=c("1","2","3","4","5","6"),
                    labels=c("Calumet", "Egan", "Hanover Park","Kirie","Lemont","Obrien"))
 print(new_bplot)
-
 
 new_hist <- ggplot(data=new_water_df, aes(new_water_df$TKN)) + 
   geom_histogram(col="black", 
@@ -58,6 +57,9 @@ new_hist <- ggplot(data=new_water_df, aes(new_water_df$TKN)) +
   labs(x="TKN(mg/L)", y="Count")
 print(new_hist)
 
+# do anova test on TKN
+anova_R <- aov(TKN ~ LOCATION, data = new_water_df)
+summary(anova_R) # f-value = 496.5
 
 # calculate critical value
 alpha_05 <- .05
@@ -68,6 +70,7 @@ criticalValue_05 = qf(p = 1 - alpha_05
 
 # H0: The mean of TKN of different location are equal.
 # H1: The mean of TKN of different location are not all equal.
-ifelse(690 >= criticalValue, "Reject H0", 
+ifelse(496.5 >= criticalValue, "Reject H0", 
                               "Fail to reject H0")
 
+# why F value can be so large
