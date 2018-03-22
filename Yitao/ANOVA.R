0f0c29d70ec9ad799caf202dae4763c443da36b4
Yitamon <claret0127@gmail.com>
2018-03-22 12:56:52 -0500

    Update ANOVA.R

--- Yitao/ANOVA.R
+++ Yitao/ANOVA.R
@@ -8,21 +8,62 @@
 
 # Read data
 # Please change the library manually to read the file.
-water_df <- read.csv("~/Desktop/completeMwrd.csv",  header=TRUE, sep=",")
-water_df <- water_df[,-1]
+water_df <- read.csv("~/Desktop/ALL_MWRD.csv",  header=TRUE, sep=",")
 head(water_df)
-row(water_df)
 
-water_df[,'Location'] <- as.factor(water_df[,'Location'])
+water_df[,'TKN'] <- as.character(water_df[,'TKN'])
+water_df[,'TKN'] <- as.numeric(water_df[,'TKN'])
 
-anova_R <- aov(TKN ~ Location, data = water_df)
+sort(water_df$TKN, decreasing = T)
+
+water_df[,'LOCATION'] <- as.factor(water_df[,'LOCATION'])
+
+library(ggplot2)
+# Location: 1=calumet, 2=egan,3=hanover park,
+#           4=kirie,5=lemont,6=obrien,7=southwest,8=west side
+bplot <- ggplot(water_df, aes(x = LOCATION, y=TKN,fill=LOCATION)) + 
+                geom_boxplot(alpha = 0.7) +
+                scale_x_discrete(breaks=c("1","2","3","4","5","6"),
+                   labels=c("Calumet", "Egan", "Hanover Park","Kirie","Lemont","Obrien"))
+print(bplot)
+
+
+hist <- ggplot(data=water_df, aes(water_df$TKN)) + 
+  geom_histogram(col="black", 
+                 aes(fill=..count..), 
+                 alpha = .7) + 
+                 labs(title="Histogram for TKN") +
+                 labs(x="TKN(mg/L)", y="Count")
+print(hist)
+
+new_water_df <- water_df[water_df[, "TKN"] <= 50,]
+
+
+anova_R <- aov(TKN ~ LOCATION, data = new_water_df)
 summary(anova_R)
 
+
+new_bplot <- ggplot(new_water_df, aes(x = LOCATION, y=TKN,fill=LOCATION)) + 
+  geom_boxplot(alpha = 0.7) +
+  scale_x_discrete(breaks=c("1","2","3","4","5","6"),
+                   labels=c("Calumet", "Egan", "Hanover Park","Kirie","Lemont","Obrien"))
+print(new_bplot)
+
+
+new_hist <- ggplot(data=new_water_df, aes(new_water_df$TKN)) + 
+  geom_histogram(col="black", 
+                 aes(fill=..count..), 
+                 alpha = .7) + 
+  labs(title="Histogram for TKN") +
+  labs(x="TKN(mg/L)", y="Count")
+print(new_hist)
+
+
 # calculate critical value
-alpha <- .05
-criticalValue = qf(p = 1 - alpha
-                   , df1 = length(unique(water_df$Location)) - 1
-                   , df2 = length(water_df$TKN) - length(unique(water_df$Location)))
+alpha_05 <- .05
+criticalValue_05 = qf(p = 1 - alpha_05
+                   , df1 = length(unique(new_water_df$LOCATION)) - 1
+                   , df2 = length(new_water_df$TKN) - length(unique(new_water_df$LOCATION)))
 
 
 # H0: The mean of TKN of different location are equal.
