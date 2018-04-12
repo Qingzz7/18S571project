e36689fee1fb8d2f1338d2ef00e361aed4570868
Yue Ning <yning5@hawk.iit.edu>
2018-04-12 14:17:11 -0500

    Update and rename project.R to summary statistic.R

--- summary statistic.R
+++ summary statistic.R
@@ -1,66 +1,21 @@
-wastewater <- read.csv("C:/Users/ZH1/Desktop/MATH571/project/completeMwrd.csv", header = TRUE)
-library(mlbench)
-wastewater <- wastewater[,-1]
-#review  first 20 rows of data
-head(wastewater, n=20)
+wastewater <- read.csv("C:/Users/ZH1/Desktop/MATH571/project/completeMwrd_03_22_2018.csv", header = TRUE)
+#library(mlbench)
+
+#review  first 5 rows of data
+head(wastewater, n=5)
+dim(wastewater)
+#drop first two cloumns
+wastewater <- subset(wastewater, select = -c(X,all.mwrd.clean.DATE) )
+head(wastewater, n=5)
 #the rows and columns
 dim(wastewater)
-# list types for each attribute
-sapply(wastewater, class)
-#       X      BOD5       TKN     NH3.N     P.TOT        SS      FLOW  Rainfall  Location 
-#"integer" "integer" "integer" "integer" "integer" "integer" "integer" "numeric" "integer" 
+str(wastewater)
 summary(wastewater)
 library(pastecs)
 library(boot)
 options(scipen=100)
 options(digits=2)
-stat.desc(wastewater[c("BOD5","TKN","NH3.N","P.TOT","SS","FLOW","Rainfall")])
-library(ggplot2)
-#histgram
-p<-ggplot(wastewater, aes(x=TKN))+
-  geom_histogram(color="black", fill="white")+
-  facet_grid(Location ~ .)
-#qq plots
-qplot(sample = TKN, data = wastewater, color=Location)+
-  labs(title="TKN according to the Location", y="mg/L concentration")
-
-table <- aggregate(wastewater[c("BOD5","TKN","NH3.N","P.TOT","SS","FLOW","Rainfall")],by=list(Locatin=wastewater$Location), mean, na.rm=TRUE)
-mean <- aggregate(. ~ Location, wastewater, mean)
-sd <- aggregate(. ~ Location, wastewater, sd)
-IQR <- aggregate(. ~ Location, wastewater, IQR)
-#standard deviation for each numeric attribute
-sapply(wastewater[,1-9],sd)
-correlations <- cor(wastewater[,1-9])
-correlations
-#correlograms
-install.packages("corrgram")
-library(corrgram)
-corrgram(wastewater, order = TRUE, lower.panel = panel.shade,
-         upper.panel = panel.pie, text.panel = panel.txt,
-         main = "Correlogram of wastewater intercorrelations")
-
-# another method
-library(ggplot2)
-install.packages("ggcorrplot")
-library(ggcorrplot)
-
-# Correlation matrix
-corr <- round(cor(wastewater), 1)
-# Plot
-ggcorrplot(corr, hc.order = TRUE, 
-           type = "lower", 
-           lab = TRUE, 
-           lab_size = 3, 
-           method="circle", 
-           colors = c("tomato2", "white", "springgreen3"), 
-           title="Correlogram of wastewater", 
-           ggtheme=theme_bw)
-
-plot.ts(wastewater,col=rainbow(ncol(wastewater)))
-head(wastewater)
-
-library(ggplot2)
-library(reshape2)
+s <- stat.desc(wastewater)
 
 wastewater$Location[wastewater$Location == "1"] <- "calumet"
 wastewater$Location[wastewater$Location == "2"] <- "egan"
@@ -71,6 +26,55 @@ wastewater$Location[wastewater$Location == "6"] <- "obrien"
 wastewater$Location[wastewater$Location == "7"] <- "southwest"
 wastewater$Location[wastewater$Location == "8"] <- "westside"
 
+library(ggplot2)
+#histgram
+ggplot(wastewater, aes(x=BOD5))+
+  geom_histogram(color="blue", fill="yellow")+
+  facet_grid(Location ~ .)
+#qqplots
+qplot(sample = BOD5, data = wastewater, color=Location)+
+  labs(title="BOD5 according to the Location", y="mg/L concentration")
+
+
+mean <- aggregate(. ~ Location, wastewater, mean)
+mean
+sd <- aggregate(. ~ Location, wastewater, sd)
+sd
+IQR <- aggregate(. ~ Location, wastewater, IQR)
+IQR
+
+#correlations <- cor(wastewater[1:7])
+#correlations
+#correlograms
+#install.packages("corrgram")
+#library(corrgram)
+#corrgram(wastewater, order = TRUE, lower.panel = panel.shade,
+#         upper.panel = panel.pie, text.panel = panel.txt,
+#         main = "Correlogram of wastewater intercorrelations")
+
+# another method
+library(ggplot2)
+#install.packages("ggcorrplot")
+library(ggcorrplot)
+
+# Correlation matrix
+corr <- round(cor(wastewater[1:7]), 1)
+corr
+# Plot
+ggcorrplot(corr, hc.order = TRUE, 
+           type = "lower", 
+           lab = TRUE, 
+           lab_size = 3, 
+           method="circle", 
+           colors = c("tomato2", "white", "springgreen3"), 
+           title="Correlogram of wastewater", 
+           ggtheme=theme_bw)
+
+#plot.ts(wastewater,col=rainbow(ncol(wastewater[1:7])))
+
+
+library(ggplot2)
+library(reshape2)
 df <- melt(wastewater, id.vars = "Location")
 #barplots for all location
 ggplot(df, aes(x = variable, y = value, color = variable)) + 
@@ -92,7 +96,6 @@ ggplot(subset(df, Location == "southwest"), aes(x = variable, y = value, color =
   geom_bar(stat = "identity", fill = "white", width = 0.8) + ggtitle("barplot of location southwest")
 ggplot(subset(df, Location == "westside"), aes(x = variable, y = value, color = variable)) + 
   geom_bar(stat = "identity", fill = "white", width = 0.8) + ggtitle("barplot of location westside")
-
 
 
 
